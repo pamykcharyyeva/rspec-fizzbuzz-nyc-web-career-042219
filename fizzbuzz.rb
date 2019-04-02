e80391db945c1ac6878c80a467d41e8538eff7c6
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-02 20:07:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
 describe "Fizz Buzz Tests" 
 do 
-  
+  def fizz_buzz (number)
+    if number%3==0&&number%5==0
+      puts ("FizzBuzz")
+    elsif number%3==0
+      puts ("Fizz")
+    elsif number%5==0
+      puts ("Buzz")
+    else
+      puts (number)
+  end
+ end
+ end
