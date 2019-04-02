deb7ddd4906d3de897f1e41a3fc4a25343c09293
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-02 20:06:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,21 @@
-def fizz_buzz (number)
-    if number%15==0
-      return ("FizzBuzz")
-    elsif number%3==0
-      return ("Fizz")
-    elsif number%5==0
-      return ("Buzz")
-    else
-      return (number)
+describe "Fizz Buzz Tests" do
+  it "should return `Fizz` for number divisible by 3" do
+    expect(fizzBuzz(3)).to eq "Fizz"
   end
- end
+
+  it "should return `Buzz` for number divisible by 5" do
+    expect(fizzBuzz(5)).to eq "Buzz"
+  end
+
+  it "should return `FizzBuzz` for number divisible by 3 and 5" do
+    expect(fizzBuzz(15)).to eq "FizzBuzz"
+  end
+
+  it "should return `FizzBuzz` for number divisible by 3 and 5" do
+    expect(fizzBuzz(90)).to eq "FizzBuzz"
+  end
+
+  it "should return 7 since it is indivisible by 3 and 5" do
+    expect(fizzBuzz(7)).to eq 7
+  end
+end
