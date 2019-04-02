24550f09a1ba89b96cc81522dd0a771cb1ce4956
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-02 20:57:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,10 @@
-require_relative './spec_helper.rb'
+describe "fizzbuzz" do
+  it 'returns "Fizz" when the number is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+
+    expect(fizz_3).to eq("Fizz")
+  end
+end
 def fizzbuzz(number)
     if (number%3 == 0) && (number%5==0)
        "FizzBuzz"
